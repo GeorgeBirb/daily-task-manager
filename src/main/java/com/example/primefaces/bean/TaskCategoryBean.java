be7483674180ad75efaaff8d74790b4bd23553dc
George Birbakos <gbirmpakos@gmail.com>
2024-02-14 01:09:05 +0200

    Enabled docx print with docx4j

--- src/main/java/com/example/primefaces/bean/TaskCategoryBean.java
+++ src/main/java/com/example/primefaces/bean/TaskCategoryBean.java
@@ -1,6 +1,12 @@
 package com.example.primefaces.bean;
 
-import Services.TaskCategoryService;
+import java.io.IOException;
+
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.Text;
+import services.TaskCategoryService;
 import lombok.Getter;
 import lombok.Setter;
 import models.TaskCategory;
@@ -8,9 +14,14 @@ import models.TaskCategory;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -27,4 +38,80 @@ public class TaskCategoryBean {
     public void init() {
         taskCategoryList = service.getAllTaskCategories();
     }
+
+    // readHere --> https://www.baeldung.com/docx4j
+    public void exportFromDocx4j() {
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        try {
+            externalContext.responseReset();
+            externalContext.setResponseContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            externalContext.setResponseHeader("Content-Disposition", "attachment; filename=\"welcome.docx\"");
+
+            OutputStream out = externalContext.getResponseOutputStream();
+            WordprocessingMLPackage wordPackage = WordprocessingMLPackage.createPackage();
+            MainDocumentPart mainDocumentPart = wordPackage.getMainDocumentPart();
+            mainDocumentPart.addStyledParagraphOfText("Title", "Hello World!");
+            mainDocumentPart.addParagraphOfText("Welcome To Baeldung");
+            wordPackage.save(out);
+        } catch (Docx4JException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources(facesContext);
+        }
+    }
+
+    public void exportFromExistingDocx() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+
+        try {
+            externalContext.responseReset();
+            externalContext.setResponseContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+            externalContext.setResponseHeader("Content-Disposition", "attachment; filename=\"ok.docx\"");
+
+            OutputStream out = externalContext.getResponseOutputStream();
+
+            try (InputStream inputStream = getClass().getResourceAsStream("/ok.docx")) {
+                WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(inputStream);
+
+                MainDocumentPart mainDocumentPart = wordMLPackage.getMainDocumentPart();
+                String textNodesXPath = "//w:t";
+                List<Object> textNodes = null;
+                try {
+                    textNodes = mainDocumentPart.getJAXBNodesViaXPath(textNodesXPath, true);
+                } catch (JAXBException e) {
+                    throw new RuntimeException(e);
+                }
+
+                for (Object obj : textNodes) {
+                    if (((Text) ((JAXBElement<?>) obj).getValue()).getValue().equals("Ok")) {
+                        ((Text) ((JAXBElement<?>) obj).getValue()).setValue("I_HAVE_CHANGED!");
+                    }
+                    Text text = (Text) ((JAXBElement) obj).getValue();
+                    String textValue = text.getValue();
+                    System.out.println(textValue);
+                }
+
+                wordMLPackage.save(out);
+            } catch (Docx4JException | IOException e) {
+                e.printStackTrace();
+            }
+
+            facesContext.responseComplete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeResources(FacesContext facesContext) {
+        try {
+            facesContext.responseComplete();
+            facesContext.getExternalContext().getResponseOutputStream().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/main/java/services/TaskCategoryService.java
@@ -0,0 +1,31 @@
+package services;
+
+import models.TaskCategory;
+
+import javax.faces.bean.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class TaskCategoryService {
+    private List<TaskCategory> taskCategoryList = new ArrayList<>();
+
+    public List<TaskCategory> getAllTaskCategories() {
+        TaskCategory taskCategory1 = new TaskCategory();
+        TaskCategory taskCategory2 = new TaskCategory();
+
+        taskCategory1.setId(1);
+        taskCategory1.setName("Study");
+        taskCategory1.setDescription("Study Description");
+
+
+        taskCategory2.setId(2);
+        taskCategory2.setName("Work Out");
+        taskCategory2.setDescription("Work Out Description");
+
+        taskCategoryList.add(taskCategory1);
+        taskCategoryList.add(taskCategory2);
+
+        return taskCategoryList;
+    }
+}
